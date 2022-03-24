a65c0e255dfdf5ddc52385c7d3a028958b8e8b08
Arjun Singh <arjunkhakh@gmail.com>
2022-03-24 16:22:10 +0000

    working on the product and tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -3,9 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const userData = await ProductTag.findAll({
+      include: [{ model: Product }, {model: Tag }]
+  });
+    res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.get('/:id', (req, res) => {
